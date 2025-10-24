26852d822b37f43f166ec30d1939b7c677f8a39e
dryflowery <sechane12@naver.com>
2025-10-24 21:32:08 +0900

    chatbot frontend refactoring

---
+++ stg/frontend/js/script.js
@@ -0,0 +1,36 @@
+// 메인 함수
+
+// 언어
+let language = "KOR";
+
+// DOM 요소 선택
+const chatbotContainer = document.getElementById("chatbot-container");
+const chatbot = document.getElementById("chatbot");
+const toggleBtn = document.getElementById("chatbot-toggle-btn");
+
+const infoModal = document.getElementById("info-modal");
+const resetModal = document.getElementById("reset-modal");
+
+const langBtn = document.getElementById("lang-btn");
+const infoBtn = document.getElementById("info-btn");
+const resetBtn = document.getElementById("reset-btn");
+const closeBtn = document.getElementById("close-btn");
+
+const langDropdown = document.getElementById('lang-dropdown');
+const langList = document.getElementById('lang-list');
+
+const sendBtn = document.getElementById("send-btn");
+const userInput = document.getElementById("user-input");
+const messages = document.getElementById("chat-messages");
+
+
+// 초기화 및 기본 이벤트
+window.addEventListener('DOMContentLoaded', () => {
+    sendDefaultMesage();
+    setModal();
+    setUserInputPlaceHolder();
+
+    setChatbotExpanded();
+    setLangDropdown();
+    setInputAndSend();
+});
