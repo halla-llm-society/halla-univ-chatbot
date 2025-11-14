b8366d3f8f09ca49845f11f9416f41498dc5ce59
Sechan Lee <83629244+dryflowery@users.noreply.github.com>
2025-11-15 00:37:47 +0900

    Update mongodb_crud.py
    자료형 수정

--- stg/backend/app/db/mongodb_crud.py
+++ stg/backend/app/db/mongodb_crud.py
@@ -1,5 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from datetime import datetime, timezone
+from bson import ObjectId
 from app.core.config import settings
 
 def _preprocess_for_db(data: dict, collectionName: str) -> tuple[dict, str]:
@@ -23,7 +24,7 @@ async def save_to_mongodb(db: AsyncIOMotorDatabase, data: dict, collection_name:
     await collection.insert_one(data)
 
 
-async def save_chat_and_return_id(db: AsyncIOMotorDatabase, data: dict, collection_name: str) -> str:
+async def save_chat_and_return_id(db: AsyncIOMotorDatabase, data: dict, collection_name: str) -> ObjectId:
     data, collection_name = _preprocess_for_db(data, collection_name)
     collection = db[collection_name]
     result = await collection.insert_one(data)
