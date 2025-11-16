f7345bfd74d08a29f6aaf06dcdbd7faab106873a
Sechan Lee <sechane12@naver.com>
2025-11-16 17:04:18 +0900

    Update log

--- stg/backend/app/services/cost_limit.py
+++ stg/backend/app/services/cost_limit.py
@@ -6,9 +6,16 @@ from app.core.config import settings
 
 logger = logging.getLogger(__name__)
 
-COST_TOTAL_KEY = "global:monthly_cost_total"
+MONTHLY_TOTAL_COST_KEY = "global:monthly_total_cost"
 TRACKING_MONTH_KEY = "global:cost_tracking_month"
 MONTHLY_LIMIT_USD = settings.MONTHLY_COST_LIMIT 
+
+
+# total cost 확인
+async def get_monthly_total_cost(redis_client: redis.Redis) -> float:
+    monthly_total_cost_str = await redis_client.get(MONTHLY_TOTAL_COST_KEY)
+    monthly_total_cost = float(monthly_total_cost_str or 0.0)
+    return monthly_total_cost
 
 
 # 비용 한도 확인
@@ -16,13 +23,10 @@ async def check_cost_limit(redis_client: redis.Redis):
     try:
         await reset_month(redis_client)
 
-        current_cost_str = await redis_client.get(COST_TOTAL_KEY)
-        current_cost = float(current_cost_str or 0.0)
+        monthly_total_cost = await get_monthly_total_cost(redis_client)
         
-        logger.info(f"Current cost: ${current_cost}")
-        
-        if current_cost >= MONTHLY_LIMIT_USD:
-            logger.warning(f"Request blocked due to exceeding monthly cost limit (${MONTHLY_LIMIT_USD}). (Current: ${current_cost})")
+        if monthly_total_cost >= MONTHLY_LIMIT_USD:
+            logger.warning(f"Request blocked due to exceeding monthly cost limit (${MONTHLY_LIMIT_USD}). (Total: ${monthly_total_cost})")
             raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS)
             
     except redis.RedisError as e:
@@ -35,21 +39,25 @@ async def reset_month(redis_client: redis.Redis):
     current_month = datetime.now(timezone.utc).strftime("%Y-%m")
     tracking_month = await redis_client.get(TRACKING_MONTH_KEY)
 
-    logger.info(f"Current month: ${current_month}, Tracking month: {tracking_month}")
-
     if tracking_month != current_month:
         logger.info(f"Reset month {tracking_month} to {current_month}")
 
         async with redis_client.pipeline() as pipe:
-            pipe.set(COST_TOTAL_KEY, 0) 
+            pipe.set(MONTHLY_TOTAL_COST_KEY, 0) 
             pipe.set(TRACKING_MONTH_KEY, current_month) 
             await pipe.execute()
 
 
 # 비용 누적 함수
-async def increment_global_cost(redis_client: redis.Redis, cost_usd: float):
+async def increment_monthly_total_cost(redis_client: redis.Redis, cost_usd: float):
+    if cost_usd < 0:
+        raise ValueError("cost_usd must be non-negative")
+
     try:
-        await redis_client.incrbyfloat(COST_TOTAL_KEY, cost_usd)
+        await redis_client.incrbyfloat(MONTHLY_TOTAL_COST_KEY, cost_usd)
+        monthly_total_cost = await get_monthly_total_cost(redis_client)
+
+        logger.info(f"Current cost: ${cost_usd}, Total cost: ${monthly_total_cost}")
     
     except Exception as e:
         logger.error(f"Failed to increase cost: {e}", exc_info=True)
