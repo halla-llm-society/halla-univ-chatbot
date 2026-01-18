7f09a98cf418fb85b7034bcf6f795a94590bcede
pwk0131 <pakr051323@gmail.com>
2026-01-18 20:22:06 +0900

    feat: Global Exception 정의

---
+++ chatbot/backend/src/main/java/com/hallachatbot/backend/global/exception/GlobalException.java
@@ -0,0 +1,14 @@
+package com.hallachatbot.backend.global.exception;
+
+import com.hallachatbot.backend.global.errorcode.ErrorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class GlobalException extends RuntimeException {
+
+	private final ErrorCode errorCode;
+
+}
